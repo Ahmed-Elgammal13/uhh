a80ac201c49ed5ac62f8fadfa9f168fc7039f01a
Ahmed-Elgammal13 <stefishot@signinid.com>
2026-02-15 21:46:47 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,31 @@
 export default {
   async fetch(request, env) {
-    // Serve all static files from the /public directory
-    return env.ASSETS.fetch(request);
+    let response = await env.ASSETS.fetch(request);
+
+    // Clone the response so we can modify headers
+    let newHeaders = new Headers(response.headers);
+
+    const url = new URL(request.url);
+
+    // Add Brotli header for .br files
+    if (url.pathname.endsWith(".br")) {
+      newHeaders.set("Content-Encoding", "br");
+
+      // Unity expects correct MIME types too
+      if (url.pathname.endsWith(".js.br")) {
+        newHeaders.set("Content-Type", "application/javascript");
+      }
+      if (url.pathname.endsWith(".wasm.br")) {
+        newHeaders.set("Content-Type", "application/wasm");
+      }
+      if (url.pathname.endsWith(".data.br")) {
+        newHeaders.set("Content-Type", "application/octet-stream");
+      }
+    }
+
+    return new Response(response.body, {
+      status: response.status,
+      headers: newHeaders
+    });
   }
 }
