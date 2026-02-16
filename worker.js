cf6893b107b2102a4dd2c3bbe7e8ee15623f5214
Ahmed-Elgammal13 <stefishot@signinid.com>
2026-02-15 21:48:37 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,31 +1,30 @@
 export default {
   async fetch(request, env) {
-    let response = await env.ASSETS.fetch(request);
-
-    // Clone the response so we can modify headers
-    let newHeaders = new Headers(response.headers);
-
     const url = new URL(request.url);
 
-    // Add Brotli header for .br files
-    if (url.pathname.endsWith(".br")) {
-      newHeaders.set("Content-Encoding", "br");
+    // Fetch the static asset
+    let response = await env.ASSETS.fetch(request);
 
-      // Unity expects correct MIME types too
+    // Clone headers so we can modify them
+    let headers = new Headers(response.headers);
+
+    // Handle Brotli-compressed Unity files
+    if (url.pathname.endsWith(".br")) {
+      headers.set("Content-Encoding", "br");
+
+      // Fix MIME types Unity expects
       if (url.pathname.endsWith(".js.br")) {
-        newHeaders.set("Content-Type", "application/javascript");
-      }
-      if (url.pathname.endsWith(".wasm.br")) {
-        newHeaders.set("Content-Type", "application/wasm");
-      }
-      if (url.pathname.endsWith(".data.br")) {
-        newHeaders.set("Content-Type", "application/octet-stream");
+        headers.set("Content-Type", "application/javascript");
+      } else if (url.pathname.endsWith(".wasm.br")) {
+        headers.set("Content-Type", "application/wasm");
+      } else if (url.pathname.endsWith(".data.br")) {
+        headers.set("Content-Type", "application/octet-stream");
       }
     }
 
     return new Response(response.body, {
       status: response.status,
-      headers: newHeaders
+      headers
     });
   }
 }
