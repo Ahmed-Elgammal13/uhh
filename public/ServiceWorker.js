5399e2fe0f521b82630b28b7cb2b1b055ac4d60d
Ahmed-Elgammal13 <stefishot@signinid.com>
2026-02-15 21:35:26 -0600

    Create ServiceWorker.js

---
+++ public/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Five Night's At SRMS-1.0";
+const contentToCache = [
+    "Build/Five Nights At SRMS.loader.js",
+    "Build/Five Nights At SRMS.framework.js.br",
+    "Build/Five Nights At SRMS.data.br",
+    "Build/Five Nights At SRMS.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
