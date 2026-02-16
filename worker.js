d8ad61294308d9ad0d70e986a64a07f4eb5c2d20
Ahmed-Elgammal13 <stefishot@signinid.com>
2026-02-15 22:06:04 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,30 +1,43 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request, env, ctx) {
     const url = new URL(request.url);
+    const pathname = url.pathname;
 
-    // Fetch the static asset
-    let response = await env.ASSETS.fetch(request);
-
-    // Clone headers so we can modify them
-    let headers = new Headers(response.headers);
-
-    // Handle Brotli-compressed Unity files
-    if (url.pathname.endsWith(".br")) {
-      headers.set("Content-Encoding", "br");
-
-      // Fix MIME types Unity expects
-      if (url.pathname.endsWith(".js.br")) {
-        headers.set("Content-Type", "application/javascript");
-      } else if (url.pathname.endsWith(".wasm.br")) {
-        headers.set("Content-Type", "application/wasm");
-      } else if (url.pathname.endsWith(".data.br")) {
-        headers.set("Content-Type", "application/octet-stream");
+    // Serve Unity build files from R2
+    if (pathname.startsWith("/Build/")) {
+      const filename = pathname.split('/').pop();
+      const key = "Build/" + filename;
+      
+      const object = await env.R2.get(key);
+      
+      if (!object) {
+        return new Response("Not found: " + key, { status: 404 });
       }
+
+      const headers = new Headers();
+      headers.set("Cache-Control", "public, max-age=31536000, immutable");
+
+      if (filename.endsWith(".js.br")) {
+        headers.set("Content-Type", "application/javascript");
+        headers.set("Content-Encoding", "br");
+      } else if (filename.endsWith(".wasm.br")) {
+        headers.set("Content-Type", "application/wasm");
+        headers.set("Content-Encoding", "br");
+      } else if (filename.endsWith(".data.br")) {
+        headers.set("Content-Type", "application/octet-stream");
+        headers.set("Content-Encoding", "br");
+      } else if (filename.endsWith(".loader.js")) {
+        headers.set("Content-Type", "application/javascript");
+        // No Content-Encoding - not compressed
+      }
+
+      return new Response(object.body, {
+        headers,
+        encodeBody: "manual"  // Prevents double compression
+      });
     }
 
-    return new Response(response.body, {
-      status: response.status,
-      headers
-    });
+    // Serve everything else from public folder
+    return env.ASSETS.fetch(request);
   }
-}
+};
